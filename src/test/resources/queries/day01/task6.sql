b3275cc1544615a01410fbe468e2043ddbb35485
Nazakat2335 <naza.beybutova.89@gmail.com>
2023-06-09 17:15:08 -0500

    Initial commit

---
+++ src/test/resources/queries/day01/task6.sql
@@ -0,0 +1,13 @@
+select * from employees;
+
+
+
+
+
+select *from EMPLOYEES ORDER BY SALARY DESC;
+select *from EMPLOYEES order by SALARY asc;
+select * from  EMPLOYEES order by 8 desc;--
+select EMAIL, SALARY from  EMPLOYEES order by SALARY desc;
+select * from EMPLOYEES where EMPLOYEE_ID<10 order by FIRST_NAME asc;
+select * from EMPLOYEES order by FIRST_NAME asc,LAST_NAME desc;
+
---
+++ src/test/resources/queries/day01/task7_like.sql
@@ -0,0 +1,11 @@
+select  * from employees;
+select * from  EMPLOYEES where FIRST_NAME like 'B%';
+select * from  EMPLOYEES where FIRST_NAME like 'B%_____';
+select * from  EMPLOYEES where FIRST_NAME like '__Z__';
+select * from  EMPLOYEES where FIRST_NAME likE '%z%';
+select * from  EMPLOYEES where LAST_NAME likE '_____';
+select * from  EMPLOYEES where FIRST_NAME likE '_a%';
+select *from EMPLOYEES where LAST_NAME like'%l';
+select *from JOBS where JOB_TITLE like'%Manager';
+
+
